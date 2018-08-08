f7008baa1c36bd4b6f96e8e588bd77b37873ce78
Howard Reith <howard.reith@gmail.com>
2018-08-08 08:26:32 -0400

    Journal form now updates with show model
    -journal-form now populates with the model content from show.js.
    -As such, update now works properly

--- app/components/journal-form.js
+++ app/components/journal-form.js
@@ -7,14 +7,14 @@ export default Component.extend({
 
   actions: {
     submit () {
-      console.log('title in journal-form is ', this.get('title'))
-      console.log('content in journal-form is ', this.get('content'))
-      console.log('starred in journal-form is ', this.get('starred'))
-      console.log('id in journal-form is ', this.get('id'))
-      this.entry.title = this.get('title')
-      this.entry.content = this.get('content')
-      this.entry.starred = this.get('starred')
-      this.entry.id = this.get('id')
+      console.log('title in journal-form is ', this.get('journalEntry.title'))
+      console.log('content in journal-form is ', this.get('journalEntry.content'))
+      console.log('starred in journal-form is ', this.get('journalEntry.starred'))
+      console.log('id in journal-form is ', this.get('journalEntry.id'))
+      this.entry.title = this.get('journalEntry.title')
+      this.entry.content = this.get('journalEntry.content')
+      this.entry.starred = this.get('journalEntry.starred')
+      this.entry.id = this.get('journalEntry.id')
       console.log('entry is ', this.get('entry'))
       this.sendAction('submit', this.get('entry'))
     },
