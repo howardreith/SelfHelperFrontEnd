d21425fe9b7538cf86932169e2978ae3a55b5408
Howard Reith <howard.reith@gmail.com>
2018-08-09 09:43:01 -0400

    Sort table functions on click
    -Table sorts by date when id is clicked

--- app/routes/journal/view.js
+++ app/routes/journal/view.js
@@ -46,6 +46,38 @@ export default Route.extend({
         this.get('flashMessages')
         .danger('There was a problem. Please try again.')
       })
+    },
+    sortTable () {
+      console.log('sort table was activated')
+      let rows, i, x, y, shouldSwitch
+      const table = document.getElementById('journal-table')
+      let switching = true
+      while (switching) {
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        console.log('rows is ', rows)
+        console.log('rows.length is ', rows.length)
+        for (i = 1; i < (rows.length - 1); i++) {
+          console.log('this ran ', i, ' times')
+          shouldSwitch = false
+          x = rows[i].getElementsByTagName('TD')[2]
+          y = rows[i + 1].getElementsByTagName('TD')[2]
+          // if (heading === 0 || heading === 3) {
+          //   if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //     shouldSwitch = true
+          //     break
+          //   }
+          // } else {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true
+          break
+        }
+      }
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+        switching = true
+      }
     }
   }
+}
 })
