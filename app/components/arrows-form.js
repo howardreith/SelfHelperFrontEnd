a635d024ce88e28c16ce0033c5774f49660a5998
howardreith <howard.reith@gmail.com>
2018-08-15 14:38:29 -0400

    Add downward arrow layer functionality
    -Added a button to add a layer that reveals the next three fields
    in the downward arrow technique.
    -Currently limited to 20 fields.

--- app/components/arrows-form.js
+++ app/components/arrows-form.js
@@ -20,6 +20,63 @@ export default Component.extend({
     },
     cancel () {
       this.sendAction('cancel')
+    },
+    show2 () {
+      document.getElementById('arrow-layer-2').setAttribute('class', '')
+    },
+    show3 () {
+      document.getElementById('arrow-layer-3').setAttribute('class', '')
+    },
+    show4 () {
+      document.getElementById('arrow-layer-4').setAttribute('class', '')
+    },
+    show5 () {
+      document.getElementById('arrow-layer-5').setAttribute('class', '')
+    },
+    show6 () {
+      document.getElementById('arrow-layer-6').setAttribute('class', '')
+    },
+    show7 () {
+      document.getElementById('arrow-layer-7').setAttribute('class', '')
+    },
+    show8 () {
+      document.getElementById('arrow-layer-8').setAttribute('class', '')
+    },
+    show9 () {
+      document.getElementById('arrow-layer-9').setAttribute('class', '')
+    },
+    show10 () {
+      document.getElementById('arrow-layer-10').setAttribute('class', '')
+    },
+    show11 () {
+      document.getElementById('arrow-layer-11').setAttribute('class', '')
+    },
+    show12 () {
+      document.getElementById('arrow-layer-12').setAttribute('class', '')
+    },
+    show13 () {
+      document.getElementById('arrow-layer-13').setAttribute('class', '')
+    },
+    show14 () {
+      document.getElementById('arrow-layer-14').setAttribute('class', '')
+    },
+    show15 () {
+      document.getElementById('arrow-layer-15').setAttribute('class', '')
+    },
+    show16 () {
+      document.getElementById('arrow-layer-16').setAttribute('class', '')
+    },
+    show17 () {
+      document.getElementById('arrow-layer-17').setAttribute('class', '')
+    },
+    show18 () {
+      document.getElementById('arrow-layer-18').setAttribute('class', '')
+    },
+    show19 () {
+      document.getElementById('arrow-layer-19').setAttribute('class', '')
+    },
+    show20 () {
+      document.getElementById('arrow-layer-20').setAttribute('class', '')
     }
   }
 })
