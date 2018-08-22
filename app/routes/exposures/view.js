02a089e249cc72961952561dc07b8d15dd10361c
howardreith <howard.reith@gmail.com>
2018-08-22 08:15:59 -0400

    Add sort function to view all exposures
    -Sort function now works with view all exposures

--- app/routes/exposures/view.js
+++ app/routes/exposures/view.js
@@ -47,8 +47,34 @@ export default Route.extend({
         .danger('There was a problem. Please try again.')
       })
     },
-    sortTable () {
-      // console.log('sort table was activated')
+    sortTableById () {
+      // console.log('sort table by ID was activated')
+      let rows, i, x, y, shouldSwitch
+      const table = document.getElementById('exposures-table')
+      let switching = true
+      while (switching) {
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        // console.log('rows is ', rows)
+        // console.log('rows.length is ', rows.length)
+        for (i = 1; i < (rows.length - 1); i++) {
+          // console.log('this ran ', i, ' times')
+          shouldSwitch = false
+          x = rows[i].getElementsByTagName('TD')[0]
+          y = rows[i + 1].getElementsByTagName('TD')[0]
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true
+            break
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+          switching = true
+        }
+      }
+    },
+    sortTableByDate () {
+      // console.log('sort table by ID was activated')
       let rows, i, x, y, shouldSwitch
       const table = document.getElementById('exposures-table')
       let switching = true
@@ -62,13 +88,120 @@ export default Route.extend({
           shouldSwitch = false
           x = rows[i].getElementsByTagName('TD')[1]
           y = rows[i + 1].getElementsByTagName('TD')[1]
-          // if (heading === 0 || heading === 3) {
-          //   if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //     shouldSwitch = true
-          //     break
-          //   }
-          // } else {
           if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true
+            break
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+          switching = true
+        }
+      }
+    },
+    sortTableByAction () {
+      // console.log('sort table by activity was activated')
+      let rows, i, x, y, shouldSwitch
+      const table = document.getElementById('exposures-table')
+      let switching = true
+      while (switching) {
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        // console.log('rows is ', rows)
+        // console.log('rows.length is ', rows.length)
+        for (i = 1; i < (rows.length - 1); i++) {
+          // console.log('this ran ', i, ' times')
+          shouldSwitch = false
+          x = rows[i].getElementsByTagName('TD')[2]
+          y = rows[i + 1].getElementsByTagName('TD')[2]
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true
+            break
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+          switching = true
+        }
+      }
+    },
+    sortTableByPrediction () {
+      // console.log('sort table by fear level was activated')
+      let rows, i, x, y, shouldSwitch
+      const table = document.getElementById('exposures-table')
+      let switching = true
+      while (switching) {
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        // console.log('rows is ', rows)
+        // console.log('rows.length is ', rows.length)
+        for (i = 1; i < (rows.length - 1); i++) {
+          // console.log('this ran ', i, ' times')
+          shouldSwitch = false
+          x = rows[i].getElementsByTagName('TD')[3]
+          y = rows[i + 1].getElementsByTagName('TD')[3]
+          // console.log('x is ', x)
+          // console.log('y is ', y)
+          // console.log('x.children[0].value is ', x.children[0].value)
+          if (+x.innerHTML < +y.innerHTML) {
+            shouldSwitch = true
+            break
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+          switching = true
+        }
+      }
+    },
+    sortTableByActual () {
+      // console.log('sort table by fear level was activated')
+      let rows, i, x, y, shouldSwitch
+      const table = document.getElementById('exposures-table')
+      let switching = true
+      while (switching) {
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        // console.log('rows is ', rows)
+        // console.log('rows.length is ', rows.length)
+        for (i = 1; i < (rows.length - 1); i++) {
+          // console.log('this ran ', i, ' times')
+          shouldSwitch = false
+          x = rows[i].getElementsByTagName('TD')[4]
+          y = rows[i + 1].getElementsByTagName('TD')[4]
+          // console.log('x is ', x)
+          // console.log('y is ', y)
+          // console.log('x.children[0].value is ', x.children[0].value)
+          if (+x.innerHTML < +y.innerHTML) {
+            shouldSwitch = true
+            break
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+          switching = true
+        }
+      }
+    },
+    sortTableBySatisfaction () {
+      // console.log('sort table by fear level was activated')
+      let rows, i, x, y, shouldSwitch
+      const table = document.getElementById('exposures-table')
+      let switching = true
+      while (switching) {
+        switching = false
+        rows = table.getElementsByTagName('TR')
+        // console.log('rows is ', rows)
+        // console.log('rows.length is ', rows.length)
+        for (i = 1; i < (rows.length - 1); i++) {
+          // console.log('this ran ', i, ' times')
+          shouldSwitch = false
+          x = rows[i].getElementsByTagName('TD')[5]
+          y = rows[i + 1].getElementsByTagName('TD')[5]
+          // console.log('x is ', x)
+          // console.log('y is ', y)
+          // console.log('x.children[0].value is ', x.children[0].value)
+          if (+x.innerHTML < +y.innerHTML) {
             shouldSwitch = true
             break
           }
