84b7d6a546b299939c297bd720e1e30e40e3c868
howardreith <howard.reith@gmail.com>
2018-09-05 13:49:02 -0400

    Add functioning create routine form
    -Routines form now works as intended. It needs some styling, but it does
    its job.

--- app/components/update-routine-form.js
+++ app/components/update-routine-form.js
@@ -38,6 +38,27 @@ export default Component.extend({
       this.entry.exercise3 = this.get('routinesEntry.exercise3')
       this.entry.reps3 = this.get('routinesEntry.reps3')
       this.entry.sets3 = this.get('routinesEntry.sets3')
+      this.entry.exercise4 = this.get('routinesEntry.exercise4')
+      this.entry.reps4 = this.get('routinesEntry.reps4')
+      this.entry.sets4 = this.get('routinesEntry.sets4')
+      this.entry.exercise5 = this.get('routinesEntry.exercise5')
+      this.entry.reps5 = this.get('routinesEntry.reps5')
+      this.entry.sets5 = this.get('routinesEntry.sets5')
+      this.entry.exercise6 = this.get('routinesEntry.exercise6')
+      this.entry.reps6 = this.get('routinesEntry.reps6')
+      this.entry.sets6 = this.get('routinesEntry.sets6')
+      this.entry.exercise7 = this.get('routinesEntry.exercise7')
+      this.entry.reps7 = this.get('routinesEntry.reps7')
+      this.entry.sets7 = this.get('routinesEntry.sets7')
+      this.entry.exercise8 = this.get('routinesEntry.exercise8')
+      this.entry.reps8 = this.get('routinesEntry.reps8')
+      this.entry.sets8 = this.get('routinesEntry.sets8')
+      this.entry.exercise9 = this.get('routinesEntry.exercise9')
+      this.entry.reps9 = this.get('routinesEntry.reps9')
+      this.entry.sets9 = this.get('routinesEntry.sets9')
+      this.entry.exercise10 = this.get('routinesEntry.exercise10')
+      this.entry.reps10 = this.get('routinesEntry.reps10')
+      this.entry.sets10 = this.get('routinesEntry.sets10')
       console.log('entry is ', this.get('entry'))
       this.sendAction('submit', this.get('entry'))
     },
@@ -46,6 +67,6 @@ export default Component.extend({
     },
     showNext () {
       this.toggleProperty(this.lastReveal + 'isShowing')
+    }
   }
-}
 })
--- app/services/routines.js
+++ app/services/routines.js
@@ -52,7 +52,28 @@ export default Service.extend({
           reps2: entry.reps2,
           exercise3: entry.exercise3,
           sets3: entry.sets3,
-          reps3: entry.reps3
+          reps3: entry.reps3,
+          exercise4: entry.exercise4,
+          sets4: entry.sets4,
+          reps4: entry.reps4,
+          exercise5: entry.exercise5,
+          sets5: entry.sets5,
+          reps5: entry.reps5,
+          exercise6: entry.exercise6,
+          sets6: entry.sets6,
+          reps6: entry.reps6,
+          exercise7: entry.exercise7,
+          sets7: entry.sets7,
+          reps7: entry.reps7,
+          exercise8: entry.exercise8,
+          sets8: entry.sets8,
+          reps8: entry.reps8,
+          exercise9: entry.exercise9,
+          sets9: entry.sets9,
+          reps9: entry.reps9,
+          exercise10: entry.exercise10,
+          sets10: entry.sets10,
+          reps10: entry.reps10
         }
       }
     })
